cb66825e43a4eca6c88bae7d7266f1b95d02659d
Kshitij Sidana <30952448+KshitijSidana@users.noreply.github.com>
2018-03-05 23:05:51 +0530

    Update rpiend.py
    added option to exit code... needed to be checked~ if string can be compared wit ==

--- rpiend.py
+++ rpiend.py
@@ -1,8 +1,11 @@
 import serial
 ser=serial.serial('/dev/ttyACM0', 9600)
 #/dev/ttyAMC0 is an example of the port.
-while 1:
+flag=1
+while flag:
 	n=input('Input instruction: ')
 	string=str(n)
+	if string == "x"
+		flag=0
 	ser.write(string)
 	print string
