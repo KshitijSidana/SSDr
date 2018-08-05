e2bebab12a8dcc3b2a4250c5daf370f47e24cfde
Akash Srivastava <akashs1698@gmail.com>
2018-08-05 22:18:44 +0530

    grayScale added

--- human tracking and following/lowerBody_tracking.py
+++ human tracking and following/lowerBody_tracking.py
@@ -9,7 +9,7 @@ cap = cv2.VideoCapture(0)
 
 while 1:
     ret, img = cap.read()
-    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(img, 1.2, 3)  # for 1.1, 5 for full body
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
