9377aea63cff6d674b27061b30e7d59e642625c7
Akash Srivastava <akashs1698@gmail.com>
2018-08-05 21:41:28 +0530

    movements added to movement.py

--- human tracking and following/movement.py
+++ human tracking and following/movement.py
@@ -1,3 +1,13 @@
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17,GPIO.OUT) # right forward
+GPIO.setup(18,GPIO.OUT) # left forward
+GPIO.setup(22,GPIO.OUT) # left backward
+GPIO.setup(23,GPIO.OUT) # right backward
+
 # these are the paramenters for ideal human image
 yCentre = 300                  # y ideal
 yHeight = 420                 # y+h
@@ -6,25 +16,43 @@ xWidth = 400                 # x+w
 area = (xCentre - xWidth) * (yCentre - yHeight)   # computing area of the block
 centerX = (xCentre + xWidth) / 2      # finding ideal x center
 centerY = (yCentre + yHeight) / 2     # finding ideal y center
-
+value=50
 def moveForward():
-    print("Moving forward")          # all forward movement code with high speed
-def stopMoving():
-    print("Movement Stopped")         # sudden movement stopped
-def moveRight():
-    print("rightward movement")       # rightward movement with less speed and curvy path  means left wheel will have higher rotation
-def moveLeft():
-    print("leftward mpvement")       # leftward movement with less speed and curvy path means right wheel will have higher rotation
+    # all forward movement code with high speed
+    GPIO.output(17,True);
+    GPIO.output(18,True);
+    GPIO.output(22,True);
+    GPIO.output(23,True);
 
+
+def stopMoving():
+    # sudden movement stopped
+    GPIO.output(17,False);
+    GPIO.output(18,False);
+    GPIO.output(22,False);
+    GPIO.output(23,False);
+
+def moveRight():
+    # rightward movement with less speed and curvy path  means left wheel will have higher rotation
+    GPIO.PWM(17,50);
+    GPIO.PWM(18,100);
+    GPIO.PWM(22,100);
+    GPIO.PWM(23,50);
+def moveLeft():
+    # leftward movement with less speed and curvy path means right wheel will have higher rotation
+    GPIO.PWM(17,100);
+    GPIO.PWM(18,50);
+    GPIO.PWM(22,50);
+    GPIO.PWM(23,100);
 def minimiseError(x, y, xw, yh):         # the function for movement decision....based on PID not total
     centerImgX = (x + xw) / 2
     centerImgY = (y + yh) / 2
     areaImg = (xw - x) * (yh - y)
-    if (areaImg < area and abs(centerX-centerImgX)<=50 and abs(centerY-centerImgX)<=50):
+    if (areaImg < area and abs(centerX-centerImgX)<=value and abs(centerY-centerImgX)<=value):
         moveForward()
     elif(areaImg>=area):
         stopMoving()
-    if(centerX-centerImgX>50):
+    if(centerX-centerImgX>value):
         moveLeft()
-    elif(centerX-centerImgX<50):
+    elif(centerX-centerImgX<value):
         moveRight()
