433c4a15ee333fe9455a88fb9a873f03c902d1d6
Akash Srivastava <akashs1698@gmail.com>
2018-08-23 20:53:38 +0530

    updated the har_cascade with threading

--- har_cascade.py
+++ har_cascade.py
@@ -1,28 +1,30 @@
-import numpy as np
 import cv2
+from multiprocessing.pool import ThreadPool
 
-# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
+def readVideo(cap):
+    return cap.read()
 
-# https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+def face_cascade1(img, num1=1.3, num2=5):
+         return face_cascade.detectMultiScale(img, num1, num2)
+
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
-#eye_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+cap = cv2.VideoCapture(0)
 
-cap = cv2.VideoCapture(1)
+pool =ThreadPool(processes=1)
+pool2=ThreadPool(processes=2)
+
 while 1:
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
+    result=pool.apply_async(readVideo,(cap,))
+    ret, img = result.get()
+    #img=cv2.pyrDown(img) #improved detection and speed
+    result2=pool2.apply_async(face_cascade1,(img,))
+    faces = result2.get() # for 1.1, 5 for full body
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        roi_gray = gray[y:y + h, x:x + w]
+        # roi_gray = gray[y:y + h, x:x + w]
         roi_color = img[y:y + h, x:x + w]
-
-      #  eyes = eye_cascade.detectMultiScale(roi_gray)
-       # for (ex, ey, ew, eh) in eyes:
-        #    cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-
+        #print(y, y + h)
+        #print(x, x + w)
     cv2.imshow('img', img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
@@ -30,4 +32,7 @@ while 1:
 
 cap.release()
 cv2.destroyAllWindows()
+
+
+
 
